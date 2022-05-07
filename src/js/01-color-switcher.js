5d9400d97d0c05ce4b5140bce932ce51675d8574
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-07 23:29:17 +0300

    complete color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
-
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+ 
+ const startSwitcherBtn = document.querySelector('[data-start]');
+ const stopSwitcherBtn = document.querySelector('[data-stop]');
+ const body = document.querySelector('body');
+ let timerId = null;
+ 
+ stopSwitcherBtn.disabled = true;
+ 
+ startSwitcherBtn.addEventListener('click', () => {
+   timerId = setInterval(() => {
+     const color = getRandomHexColor();
+     body.style.backgroundColor = color;
+   }, 1000);
+   startSwitcherBtn.disabled = true;
+   stopSwitcherBtn.disabled = false;
+ });
+ 
+ stopSwitcherBtn.addEventListener('click', () => {
+   clearInterval(timerId);
+   startSwitcherBtn.disabled = false;
+   stopSwitcherBtn.disabled = true;
+ });
