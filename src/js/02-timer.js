1fd06a8f1bc78944a261c8e1e04a6203742c4faf
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-08 22:24:17 +0300

    add notiflix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,26 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-flatpickr("#datetime-picker", {
-   enableTime: true,
-   time_24hr: true,
-   defaultDate: new Date(),
-   minuteIncrement: 1,
-   onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-});
+const startBtn = document.querySelector('[data-start]');
+
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', () => {});
