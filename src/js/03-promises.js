9f2cb801ce1ed0b5ceeb371bd4d85cd107747224
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-09 21:48:38 +0300

    add input event listener

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,18 @@
+import Notiflix from 'notiflix';
+
+const generatorForm = document.querySelector('.form');
+let formData = {};
+
+const inputHandler = e => {
+   formData[e.target.name] = e.target.value;
+   console.log(formData);
+}
+
+const position = formData.step;
+const delay = formData.delay;
+const amount = formData.amount;
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +21,13 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise(position,delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+generatorForm.addEventListener('input', inputHandler);
